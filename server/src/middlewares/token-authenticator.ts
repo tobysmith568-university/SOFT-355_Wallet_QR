6bbc278f136c8c7c3fc76dd37cb4e1c4dfbc616a
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-05 16:54:26 +0000

    Implemented the token authenticator middleware

--- server/src/middlewares/token-authenticator.ts
+++ server/src/middlewares/token-authenticator.ts
@@ -1,12 +1,41 @@
 import { Request, Response, NextFunction, Express } from "express";
+import { ITokenService } from "../services/token.service.interface";
 
 export class TokenAuthenticator {
 
-  constructor() {
+  constructor(private readonly tokenService: ITokenService) {
   }
 
   public middleware(app: Express) {
     return async (req: Request, res: Response, next: NextFunction) => {
+      
+      const authToken = req.headers.authorization;
+  
+      if (authToken === undefined) {
+        res.json({ error: "You are not authenticated" });
+        res.statusCode = 401;
+        return;
+      }
+  
+      const parts = authToken.split(" ");
+  
+      if (parts.length !== 2 || parts[0] !== "Bearer") {
+        res.json({ error: "Your authorization header is not a bearer token" });
+        res.statusCode = 401;
+        return;
+      }
+
+      const username = await this.tokenService.verify(parts[1]);
+
+      if (username === null) {
+        res.json({ error: "Your token is invalid" });
+        res.statusCode = 401;
+        return;
+      }
+
+      req.username = username;
+  
+      next();
     };
   }
 }
