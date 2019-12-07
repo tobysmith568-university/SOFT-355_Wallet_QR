7cf87538eda2c79f7df4e59020f874b179a9e4fb
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-07 14:55:16 +0000

    Revert "Added QR route implementation"
    This reverts commit f2b4e9bca086889f64fd9d451ad349cc2453ea13.

--- server/src/api/routes/qr.route.ts
+++ server/src/api/routes/qr.route.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
 import { IRoute } from "./route.interface";
-import { QRController } from "../../contollers/qr.controller";
 
 export class QRRoute implements IRoute {
 
-  constructor(private expressRouter: Router,
-              private controller: QRController) {}
+  constructor() {}
 
   public setupRoutes(): void {
-    this.expressRouter.get("/qr/:data", this.controller.get);
   }
   
   public getRouter(): Router {
-    return this.expressRouter;
+    throw Error();
   }
 }
--- server/src/contollers/qr.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export class QRController {
-
-  constructor() { }
-
-  public get = async (req: Request, res: Response) => {
-  }
-}
