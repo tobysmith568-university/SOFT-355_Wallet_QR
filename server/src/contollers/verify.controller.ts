752effb7724a0e0a8fefda349016bcaf00333213
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-19 15:41:38 +0000

    Added the verify controller tests

--- server/src/contollers/verify.controller.ts
+++ server/src/contollers/verify.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
+import { ITokenService } from "../services/token.service.interface";
+import { UserRepository } from "../database/repositories/user.repository";
 
 export class VerifyController {
+
+  constructor(private readonly tokenService: ITokenService,
+              private readonly userRepository: UserRepository) { }
+
   public verify = async (req: Request, res: Response) => {
+
   }
 }
